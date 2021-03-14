68fe988231e37ad848086e84d944c8b51de21dcc
Irfan Tito Kurniawan <42381907+titoirfan@users.noreply.github.com>
2021-03-14 21:40:10 +0700

    Update soal-01.c

--- soal-01.c
+++ soal-01.c
@@ -1,44 +1,153 @@
-/** EL2208 Praktikum Pemecahan Masalah dengan C 2020/2021
-*   Modul               : 4
-*   Soal                : 1
-*   Hari dan Tanggal    : Senin, 15 Maret 2021
-*   Nama (NIM)          : 
-*   Asisten (NIM)       : 
-*   Nama File           : soal-01.c
-*   Deskripsi           : Deskripsi file ini.
-*/
-
-//fungsi dan cara penyelesaian dibebaskan namun harus menggunakan tipe bentukan berikut
-
 #include <stdio.h>
 #include <stdlib.h>
 
-//Tipe bentukkan data asam amino yang terdiri dari :
-//BN1 -> Basa nitrogen pertama
-//BN2 -> Basa nitrogen kedua
-//BN3 -> Basa nitrogen ketiga
-//Kodon -> Singkatan asam amino dari 3 basa nitrogen diatas
-//Untuk penggunaannya juga dibebaskan
+char Con, ganti;
+char *BN,*BNmutasi;
+int jumlah,i, urut,jumlahcek, bagi, modu,sum, mutasiTA;
+
+int cekKodonStart (char *BN);
+int cekKodonStop(int jum, char *BN);
+
 typedef struct dataAsMin {
-    char BN1, BN2, BN3, kodon;
+char BN1, BN2, BN3, kodon;
 } dataAsMin;
 
-int main() {
-    
-    // Template print
-    // printf("Masukkan 3 basa nitrogen : ");
-    // printf("Masukkan singkatan asam amino : ");
-    // printf("Apakah ingin lanjut? (Y/N) : ");
-    // printf("Masukkan 3 basa nitrogen untuk dicek : ");
-    // printf("Masukkan basa nitrogen lagi? (Y/N) : ");
-    // printf("Urutan basa input : ");
-    // printf("\nUrutan yang ingin diganti ? ");
-    // printf("Basa nitrogen pengganti ? ");
-    // printf("Urutan basa output : ");
-    // printf("\nTerjadi mutasi tanpa arti");
-    // printf("\nTidak terjadi mutasi tanpa arti");
-    // printf("\nMutasi harus dimulai dengan kodon start dan diakhiri dengan kodon stop!");
-    // printf("\nHarus dimulai dengan kodon start dan diakhiri dengan kodon stop!");
-    // printf("\nAsMin %c pada untaian ada %d", asam_amino.kodon, jumlah);
-    return 0;
+dataAsMin *libAsMin;
+int k, jumlahlib, jmlh;
+
+void cekKodonData (dataAsMin *libAsMin, char *BN, int jumlib, int jum);
+
+int main(){
+
+    libAsMin = (dataAsMin*)calloc(0, sizeof(dataAsMin));
+do{
+   jmlh ++;
+   libAsMin = realloc(libAsMin,jmlh * sizeof(dataAsMin));
+   printf("Masukkan 3 basa nitrogen : ");
+   scanf(" %c",&libAsMin[jmlh-1].BN1);
+   scanf(" %c",&libAsMin[jmlh-1].BN2);
+   scanf(" %c",&libAsMin[jmlh-1].BN3);
+   printf("Masukkan singkatan asam amino : ");
+   scanf(" %c",&libAsMin[jmlh-1].kodon);
+   printf("Apakah ingin lanjut? (Y/N) : ");
+   scanf(" %c", &Con);
+} while (Con != 'N');
+
+    Con = 'Y';
+    BN = (char*)calloc(0, sizeof(char));
+
+do{
+    jumlah += 3;
+    BN = (char*)realloc(BN, jumlah);
+    printf("Masukkan 3 basa nitrogen untuk dicek : ");
+    for(i=(jumlah-3) ; i < (jumlah) ; i++){
+        scanf(" %c",&BN[i]);
+        //printf("ini %c\n",BN[i]);
+    }
+    printf("Masukkan basa nitrogen lagi? (Y/N) : ");
+    scanf(" %c", &Con);
+} while (Con != 'N');
+
+    printf("Urutan basa input : ");
+    for(i=0;i<jumlah;i++){
+        printf("%c",BN[i]);
+    }
+    if ((cekKodonStart(BN)==1)&&(cekKodonStop(jumlah,BN)==1)){
+    //dimulai dari 1
+    printf("\nUrutan yang ingin diganti ? ");
+    scanf (" %d", &urut);
+    printf("Basa nitrogen pengganti ? ");
+    scanf (" %c", &ganti);
+
+    BNmutasi = (char*)calloc(jumlah, sizeof(char));
+    for (int m=0; m<jumlah;m++){
+        if (m != urut-1){
+            BNmutasi[m] = BN[m];
+        } else {
+            BNmutasi[m] = ganti;
+        }
+    }
+
+    printf("Urutan basa output : ");
+    for(i=0;i<jumlah;i++){
+        printf("%c",BNmutasi[i]);
+    }
+
+    if((cekKodonStart(BNmutasi) == 1)&&(cekKodonStop(jumlah, BNmutasi))){
+    //untuk ngecek urutan kodon mutasi
+    modu = urut%3;
+    bagi = urut/3;
+    if(modu != 0){
+        sum = (bagi + 1)*3;
+    } else {
+        sum = bagi*3;
+    }
+
+    if(sum != jumlah){
+    if ((cekKodonStop(sum, BNmutasi))==1){
+        printf("\nTerjadi mutasi tanpa arti");
+    } else {
+        printf("\nTidak terjadi mutasi tanpa arti");
+    }
+        cekKodonData(libAsMin,BNmutasi,jmlh,jumlah);
+    }else {
+        printf("\nTidak terjadi mutasi tanpa arti");
+        cekKodonData(libAsMin,BNmutasi,jmlh,jumlah);
+    }
+
+    } else {
+    printf ("\nMutasi harus dimulai dengan kodon start dan diakhiri dengan kodon stop!");
+    }
+    } else {
+    printf ("\nHarus dimulai dengan kodon start dan diakhiri dengan kodon stop!");
+    }
+}
+
+int cekKodonStart (char *BN){
+    if (BN[0] == 'A'){
+        if (BN[1] == 'U'){
+            if (BN[2] == 'G'){
+                return (1);
+            }
+        }
+    } else {
+        return (0);
+    }
+}
+
+int cekKodonStop (int jum, char *BN){
+    if (BN[jum-3]== 'U'){
+        if (BN[jum-2]=='A'){
+            if ((BN[jum-1] == 'A')||(BN[jum-1] == 'G')){
+                return (1);
+            } else {
+                return (0);
+            }
+        } else if (BN[jum-2]=='G'){
+            if (BN[jum-1]=='A'){
+                return (1);
+            } else {
+                return (0);
+            }
+        }
+    } else {
+        return (0);
+    }
+}
+
+void cekKodonData (dataAsMin *libAsMin, char *BNmutasi, int jumlib, int jum){
+    int itung = 0;
+    for (int j = 0; j<jumlib; j++){
+        for(int k=1; k<jum; k+=3){
+         if (libAsMin[j].BN1 == BNmutasi[k-1]){
+            if(libAsMin[j].BN2 == BNmutasi[k]){
+                if(libAsMin[j].BN3 == BNmutasi[k+1]){
+                    itung += 1;
+                }
+            }
+         }
+        }
+    printf ("\nAsMin %c pada untaian ada %d",libAsMin[j].kodon, itung);
+    itung = 0;
+    }
 }
